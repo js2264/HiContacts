504512407e63a3af9ecc5e29dbf2fa4d5ed99504
js2264 <jacquesserizay@gmail.com>
2021-07-23 17:15:47 +0200

    feat: parse hicstuff log

--- R/utils.R
+++ R/utils.R
@@ -115,3 +115,80 @@ splitCoords <- function(coords) {
         )
     }
 }
+
+#' getHicStats
+#'
+#' @param hicstuff_log hicstuff_log
+#'
+#' @import stringr
+#' @export
+
+getHicStats <- function(hicstuff_log) {
+    stats <- list()
+
+    ## -- Parse log file
+    nFiltered <- readLines(hicstuff_log) %>%
+        grep("INFO :: .* pairs kept", ., value = TRUE) %>%
+        stringr::str_replace_all("INFO ::| pairs.*", "") %>%
+        as.numeric()
+    nDangling <- readLines(hicstuff_log) %>%
+        grep("INFO :: .* pairs discarded", ., value = TRUE) %>%
+        stringr::str_replace_all(".* Uncuts: |, Weirds.*", "") %>%
+        as.numeric() # "Uncut"
+    nSelf <- readLines(hicstuff_log) %>%
+        grep("INFO :: .* pairs discarded", ., value = TRUE) %>%
+        stringr::str_replace_all(".* Loops: |, Uncuts.*", "") %>%
+        as.numeric() # "loop"
+    nDumped <- readLines(hicstuff_log) %>%
+        grep("INFO :: .* pairs discarded", ., value = TRUE) %>%
+        stringr::str_replace_all(".* Weirds:", "") %>%
+        as.numeric() # "Weird"
+    nDups <- readLines(hicstuff_log) %>%
+        grep("INFO :: .* PCR duplicates have", ., value = TRUE) %>%
+        stringr::str_replace_all(".*out \\(| \\/ .*", "") %>%
+        as.numeric()
+    nCis <- readLines(hicstuff_log) %>%
+        grep("INFO :: Proportion of inter contacts", ., value = TRUE) %>%
+        stringr::str_replace_all(".*intra:|, inter:.*", "") %>%
+        as.numeric()
+    nTrans <- readLines(hicstuff_log) %>%
+        grep("INFO :: Proportion of inter contacts", ., value = TRUE) %>%
+        stringr::str_replace_all(".* inter: |\\)", "") %>%
+        as.numeric()
+
+    stats[["Threshold for dangling pairs (uncut)"]] <- readLines(hicstuff_log) %>%
+        grep("INFO :: Filtering with thresholds", ., value = TRUE) %>%
+        stringr::str_replace_all(".*thresholds: | loops=.*", "") %>%
+        stringr::str_replace(".*=", "") %>%
+        as.numeric()
+    stats[["Threshold for self pairs (loop)"]] <- readLines(hicstuff_log) %>%
+        grep("INFO :: Filtering with thresholds", ., value = TRUE) %>%
+        stringr::str_replace_all(".*=", "") %>%
+        as.numeric()
+    stats[["nFragments"]] <- readLines(hicstuff_log) %>%
+        grep("INFO :: .* mapped with Q", ., value = TRUE) %>%
+        stringr::str_replace_all(".*/|\\)", "") %>%
+        as.numeric() %>%
+        `/`(2)
+    stats[["nFragments"]] <- readLines(hicstuff_log) %>%
+        grep("INFO :: .* mapped with Q", ., value = TRUE) %>%
+        stringr::str_replace_all(".*/|\\)", "") %>%
+        as.numeric() %>%
+        `/`(2)
+    stats[["nPairs"]] <- nFiltered + nDangling + nSelf + nDumped
+    stats[["pctPairs"]] <- round(stats[["nPairs"]] / stats[["nFragments"]], 4) * 100
+    stats[["nFiltered"]] <- nFiltered
+    stats[["pctFiltered"]] <- round(stats[["nFiltered"]] / stats[["nPairs"]], 4) * 100
+    stats[["nDangling"]] <- nDangling # "Uncut"
+    stats[["pctDangling"]] <- round(stats[["nDangling"]] / stats[["nPairs"]], 4) * 100
+    stats[["nSelf"]] <- nSelf # "loop"
+    stats[["pctSelf"]] <- round(stats[["nSelf"]] / stats[["nPairs"]], 4) * 100
+    stats[["nDumped"]] <- nDumped # "Weird"
+    stats[["pctDumped"]] <- round(stats[["nDumped"]] / stats[["nPairs"]], 4) * 100
+    stats[["nUnique"]] <- nFiltered - nDups
+    stats[["pctUnique"]] <- round(stats[["nUnique"]] / stats[["nPairs"]], 4) * 100
+    stats[["CisTransRatio"]] <- round(nCis / nTrans, 4)
+    stats[["CisTransPct"]] <- round(nCis / (nCis + nTrans), 4) * 100
+
+    return(stats)
+}
