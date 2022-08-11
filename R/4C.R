81675a7489f70852c43d978f8e5cd2125e3869e0
js2264 <jacquesserizay@gmail.com>
2022-08-11 18:01:40 +0200

    feat: 4C calculation and plot

--- R/4C.R
+++ R/4C.R
@@ -4,8 +4,29 @@ virtual4C <- function(x, viewpoint, use.assay = 'balanced') {
     regions <- regions(gis)
     regions_in_viewpoint <- seq_along(regions) %in% S4Vectors::queryHits(findOverlaps(regions, viewpoint))
     score <- rowSums(cm[, regions_in_viewpoint], na.rm = TRUE)
-    regions$viewpoint <- as.character(viewpoint)
-    regions$score <- score
-    return(regions)
+    tibble::tibble(
+        viewpoint = as.character(viewpoint), 
+        chr = as.vector(seqnames(regions)), 
+        center = start(regions) + (end(regions) - start(regions))/2, 
+        score = score
+    )
 }
 
+#' gg4C
+#'
+#' @param ... ...
+#'
+#' @import ggplot2
+#' @import tibble
+#' @import dplyr
+#' @export
+
+gg4C <- function(x, aes) {
+    p <- ggplot(x, aes) + 
+        geom_line() + 
+        theme_minimal() + 
+        theme(panel.border = element_rect(fill = NA)) + 
+        theme(panel.grid.minor = element_blank()) +
+        labs(x = "Position", y = "Contacts with viewpoint")
+    p
+}
