1d964532edefd93f2b77a2b602371ed59f78fcb4
js2264 <jacquesserizay@gmail.com>
2022-08-31 12:39:43 +0200

    add toy datasets

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' Example of a `contacts` object
+#'
+#' @source HiContactsData
+#' @source 
+#' fpath <- HiContactsData::HiContactsData('yeast_wt', 'mcool')
+#' contacts_yeast <- contacts(fpath, 'II', resolution = 1000)
+"contacts_yeast"
+
+#' Example of a `contacts` object
+#'
+#' @source HiContactsData
+#' @source 
+#' fpath <- HiContactsData::HiContactsData('yeast_eco1', 'mcool')
+#' contacts_yeast_eco1 <- contacts(fpath, 'II', resolution = 1000)
+"contacts_yeast_eco1"
+
+#' Example of a `contacts` object
+#'
+#' @source HiContactsData
+#' @source 
+#' fpath <- HiContactsData::HiContactsData('yeast_wt', 'mcool')
+#' full_contacts_yeast <- contacts(fpath, resolution = 16000)
+"full_contacts_yeast"
