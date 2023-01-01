f10ca426162bca66b9d0979c29493fae8aae3740
js2264 <jacquesserizay@gmail.com>
2023-01-01 23:50:00 +0100

    improve computing speed for diamond score: now only works on already imported interactions (directly found in the input HiCExperiment object -> no more parsing of cool/hic/hicpro files

--- R/diamond.R
+++ R/diamond.R
@@ -1,60 +1,70 @@
-getDiamondInsulation <- function(x, resolution, window_size = 10 * resolution, BPPARAM, ...) {
+#' @title Contact map insulation
+#' @name diamond
+#' @rdname diamond
+#' @description
+#' 
+#' Computes diamond insulation score along the entire genome 
+#'
+#' @param x A `HiCExperiment` object over a full genome
+#' @param resolution Which resolution to use to compute diamond insulation score
+#' @param window_size Which window size to use to compute diamond insulation 
+#' score (default: 10 * resolution)
+#' @param weak_threshold Less stringent cutoff to call borders in the 
+#' diamond insulation score
+#' @param strong_threshold More stringent cutoff to call borders in the 
+#' diamond insulation score
+#' @param BPPARAM BiocParallel parallelization settings 
+#' @return a `HiCExperiment` object with additional `insulation` metadata, 
+#' containing the diamond insulation score computed 
+#'
+#' @importFrom Matrix sparseMatrix
+#' @export
+#' @examples 
+#' library(HiContacts)
+#' full_contacts_yeast <- full_contacts_yeast()
+#' diams <- getDiamondInsulation(full_contacts_yeast, resolution = 1000)
+#' getDiamondInsulation(diams)
+
+getDiamondInsulation <- function(
+    x, 
+    resolution = NULL, 
+    window_size = NULL, 
+    BPPARAM = BiocParallel::bpparam()
+) {
 
     message( "Going through preflight checklist..." )
-    # - Get si and bins from contact matrix
-    file <- fileName(x)
+    # - Check resolutions 
+    if(is.null(resolution)) resolution <- resolution(x)
+    if(is.null(window_size)) window_size <- 8 * resolution
     si <- seqinfo(x)
-    if (is_cool(file) | is_mcool(file)) {
-        all_bins <- .getCoolAnchors(file, resolution = resolution)
-        si <- .cool2seqinfo(file, resolution)
-    }
-    else if (is_hic(file)) {
-        all_bins <- .getHicAnchors(file, resolution = resolution)
-        si <- .hic2seqinfo(file)
-    }
-    else if (is_hicpro_matrix(file) & is_hicpro_regions(...)) {
-        all_bins <- .getHicproAnchors(...)
-        si <- .hicpro2seqinfo(file)
-    }
+    if(is.null(ranges)) ranges <- GenomicRanges::GRanges(si)
+    ress <- resolutions(x)
+    if (!resolution %in% ress) stop("Chosen resolution not available")
+    if (!window_size %in% ress) stop("Chosen window_size not available")
     fact <- window_size / resolution
-    # - Filter non-relevant bins
-    .bins <- lapply(seqnames(si), function(chr) {
-        all_bins[seqnames(all_bins) == chr] |> 
-            head(-(fact+1)) |> tail(-(fact+1))
-    })
-    bins <- do.call(c, .bins)
-    bins <- bins[1:400]
-
-    # - !!! HEAVY LOAD !!! Parse ALL pixels and convert to sparse matrix
-    # - Full parsing has to be done since parallelized access to HDF5 is not supported 
-    # - Once full parsing is done, parallelization is trivial
-    message( "Parsing the entire contact matrice as a sparse matrix..." )
-    if (is_cool(file) | is_mcool(file)) {
-        l <- .dumpCool(file, resolution = resolution)
-    }
-    else if (is_hic(file)) {
-        l <- .dumpHic(file, resolution = resolution)
-    }
-    else if (is_hic_pro(file)) {
-        l <- .dumpHicpro(file, ...)
-    }
-    l <- Matrix::sparseMatrix(
-        i= l[['pixels']]$bin1_id + 1,
-        j= l[['pixels']]$bin2_id + 1,
-        x= l[['pixels']]$count
-    )
+    cm <- gi2cm(interactions(x), 'balanced')
+    cm_mat <- as.matrix(cm)
 
     # - Scan each bin individually
     message( "Scan each window and compute diamond insulation score..." )
+    idx_ids <- seq(fact+1, nrow(cm)-fact-1)
     scores <- BiocParallel::bplapply(BPPARAM = BPPARAM, 
-        seq_along(bins), function(K) {
-            .getDiamondScore(l, bins[K])
+        idx_ids, 
+        function(idx) {
+            # Extract matrix from chr-level, centered on idx
+            mat <- cm_mat[seq(idx-fact, idx+fact), seq(idx-fact, idx+fact)]
+            # Extract top-right quadrant
+            mat_trq <- mat[seq(1, fact) + 1 - 1, seq(fact, fact + fact - 1) + 1 + 1]
+            sum(mat_trq,na.rm = TRUE)
         }
     ) |> unlist()
-    bins$insulation <- log2( scores / mean(scores) )
-    fbins <- bins[!is.na(bins$insulation)]
+    fbins <- regions(cm)[idx_ids]
+    fbins$score <- scores
+    fbins$insulation <- log2( scores / mean(scores) )
+    fbins <- fbins[!is.na(fbins$insulation)]
 
     # - Annotate boundaries
+    message( "Annotating diamond score prominence for each window..." )
     mins <- which(diff(sign(diff(fbins$insulation)))==+2)+1
     maxs <- which(diff(sign(diff(fbins$insulation)))==-2)+1
     if (length(maxs) < length(mins)) maxs[[length(maxs)+1]] <- length(fbins)
@@ -82,13 +92,10 @@ getBorders <- function(x, weak_threshold = 0.2, strong_threshold = 0.5) {
 
 }
 
-.getDiamondScore <- function(l, bin) {
+.getDiamondScore <- function(cm, bin, fact) {
     ids <- seq(bin$bin_id - fact, bin$bin_id + fact)
-    weights <- all_bins[ids+1]$weight
-    counts <- l[ids+1, ids+1]
-    balanced <- t(apply(
-        apply(counts, 1, `*`, weights), 2, `*`, weights
-    ))
+    idx_ids <- seq(fact+1, nrow(cm_mat)-fact-1)
+    balanced <- l[ids+1, ids+1]
     s <- sum(
         balanced[seq(1, fact) + 1 - 1, seq(fact, fact + fact - 1) + 1 + 1],
         na.rm = TRUE
